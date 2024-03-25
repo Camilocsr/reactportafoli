eaa3ceb4dfef85f45bab579fc68a224e965911ed
Camilocsr <esmunred@gmail.com>
2024-03-25 17:38:33 -0500

    react

--- src/Components/PaginaError.jsx
+++ src/Components/PaginaError.jsx
@@ -1,16 +1,21 @@
-import React, { useRef } from "react";
+import React, { Suspense, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Personaje } from "./Personaje";
 import { OrbitControls } from "@react-three/drei";
+import { PointLight } from "three";
 
 const Pagina404 = () => {
   return (
     <div className="pagina-404-container">
-      <h1 className="pagina-404-title">Bienvenido a la página 404</h1>
+      <h1 className="pagina-404-title">404</h1>
       <div className="canvas-container">
         <Canvas camera={{ position: [50, 50, 50] }}>
           <ambientLight intensity={1} />
-          <Personaje scale={[30, 30, 30]} position={[0, -10, 0]} />
+          {/* <PointLight position={[35,35,0]} intensity={0.4}/>
+          <PointLight position={[-35,35,0]} intensity={0.4}/> */}
+          <Suspense fallback={null}>
+            <Personaje scale={[30, 30, 30]} position={[0, -10, 0]} />
+          </Suspense>
           <Controls />
         </Canvas>
       </div>
